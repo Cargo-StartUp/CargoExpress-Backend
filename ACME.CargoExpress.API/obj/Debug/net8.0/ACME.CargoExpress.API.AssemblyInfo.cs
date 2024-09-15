bba5eb99709b3a2bbb9ea689f143bf39db0e9372
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-09-15 17:34:34 -0500

    feat: Added extensions

--- ACME.CargoExpress.API/obj/Debug/net8.0/ACME.CargoExpress.API.AssemblyInfo.cs
+++ ACME.CargoExpress.API/obj/Debug/net8.0/ACME.CargoExpress.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86ad8fdb186fbc1affde59389ebb2e25360a4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b10f3597d0e9bb9055b935b8758a1eb047639d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ACME.CargoExpress.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
