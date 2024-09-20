bc947681aa4777639a1a1c43fae3fe861c277854
Andre Gabriel Valverde Mozo <u202218899@upc.edu.pe>
2024-09-20 18:02:38 -0500

    feat: Test1

--- ACME.CargoExpress.API/Program.cs
+++ ACME.CargoExpress.API/Program.cs
@@ -185,10 +185,15 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 
 app.UseHealthChecks("/health");
